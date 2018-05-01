3afb91bb3d2b4e618d0386013d543665839c4091
Tech-A44 <tech-a44@Tech-A44s-MacBook-Air.local>
2018-05-01 14:12:48 +0100

    remove sum when deselected done

--- js/index.js
+++ js/index.js
@@ -1,90 +1,172 @@
 
-// Each column as array of integers
-var colOne = [];
-for(var i = 0; i < 5; i++){
-  colOne.push(parseInt(document.getElementsByClassName('col-1')[i].innerHTML));
-}
-var colTwo = [];
-for(var i = 0; i < 5; i++){
-  colTwo.push(parseInt(document.getElementsByClassName('col-2')[i].innerHTML));
-}
-var colThree = [];
-for(var i = 0; i < 5; i++){
-  colThree.push(parseInt(document.getElementsByClassName('col-3')[i].innerHTML));
-}
-var colFour = [];
-for(var i = 0; i < 5; i++){
-  colFour.push(parseInt(document.getElementsByClassName('col-4')[i].innerHTML));
-}
-var colFive = [];
-for(var i = 0; i < 5; i++){
-  colFive.push(parseInt(document.getElementsByClassName('col-5')[i].innerHTML));
-}
-var colNumber = [colOne, colTwo, colThree, colFour, colFive];
 
-// Each row as array of integers
-var rowOne = [];
-for(var i = 0; i < 5; i++){
-  rowOne.push(parseInt(document.getElementsByClassName('row-1')[i].innerHTML));
-}
-var rowTwo = [];
-for(var i = 0; i < 5; i++){
-  rowTwo.push(parseInt(document.getElementsByClassName('row-2')[i].innerHTML));
-}
-var rowThree = [];
-for(var i = 0; i < 5; i++){
-  rowThree.push(parseInt(document.getElementsByClassName('row-3')[i].innerHTML));
-}
-var rowFour = [];
-for(var i = 0; i < 5; i++){
-  rowFour.push(parseInt(document.getElementsByClassName('row-4')[i].innerHTML));
-}
-var rowFive = [];
-for(var i = 0; i < 5; i++){
-  rowFive.push(parseInt(document.getElementsByClassName('row-5')[i].innerHTML));
-}
-var rowNumber = [rowOne, rowTwo, rowThree, rowFour, rowFive];
-
-// Variable for the outside-sum (outermost cells)
-var outsideSum = document.getElementsByClassName('outside-sum');
-
-// sum for top outsideSum (row 1 of 9x9)
-for (var i = 0; i < outsideSum.length/4; i++) {
-  outsideSum[i].innerHTML = colNumber[i].reduce(function(a, b) { return a + b; });
-}
-
-// sum for left outsideSum (column 1 of 9x9)
-var i,j;
-for (i = 5, j = 0; i < outsideSum.length*3/4, j < 5; i+=2, j++) {
-  outsideSum[i].innerHTML = rowNumber[j].reduce(function(a, b) { return a + b; });
-}
-
-// sum for bottom outsideSum (row 9 of 9x9)
-outsideSum[15].innerHTML = outsideSum[0].innerHTML;
-outsideSum[16].innerHTML = outsideSum[1].innerHTML;
-outsideSum[17].innerHTML = outsideSum[2].innerHTML;
-outsideSum[18].innerHTML = outsideSum[3].innerHTML;
-outsideSum[19].innerHTML = outsideSum[4].innerHTML;
-
-// sum for right outsideSum (column 9 of 9x9)
-outsideSum[6].innerHTML = outsideSum[5].innerHTML;
-outsideSum[8].innerHTML = outsideSum[7].innerHTML;
-outsideSum[10].innerHTML = outsideSum[9].innerHTML;
-outsideSum[12].innerHTML = outsideSum[11].innerHTML;
-outsideSum[14].innerHTML = outsideSum[13].innerHTML;
-
-
-
-// Selecting/Deselecting inside numbers
-var insideNumber = document.getElementsByClassName('inside');
-
-for (var i = 0; i < insideNumber.length; i++) {
-  insideNumber[i].addEventListener('click', function(event){
-    if (this.classList.contains('inside-deselect')) { //select
-      this.classList.remove('inside-deselect');
-    }else{ //deselect
-      this.classList.add('inside-deselect');
-      this.classListremove('')
+function getColumnArrays(){ //makes an array of column arrays
+  // Each column as array of integers
+  var colOne = [];
+  var col1 = document.getElementsByClassName('col-1');
+  for(var i = 0; i < 5; i++){
+    if (col1[i].classList.contains('inside-deselect')) {
+      colOne.push(0);
+    }else {
+      colOne.push(parseInt(col1[i].innerHTML));
     }
-  });
+  }
+  var colTwo = [];
+  var col2 = document.getElementsByClassName('col-2');
+  for(var i = 0; i < 5; i++){
+    if (col2[i].classList.contains('inside-deselect')) {
+      colTwo.push(0);
+    }else {
+      colTwo.push(parseInt(col2[i].innerHTML));
+    }
+  }
+  var colThree = [];
+  var col3 = document.getElementsByClassName('col-3');
+  for(var i = 0; i < 5; i++){
+    if (col3[i].classList.contains('inside-deselect')) {
+      colThree.push(0);
+    }else {
+      colThree.push(parseInt(col3[i].innerHTML));
+    }
+  }
+  var colFour = [];
+  var col4 = document.getElementsByClassName('col-4');
+  for(var i = 0; i < 5; i++){
+    if (col4[i].classList.contains('inside-deselect')) {
+      colFour.push(0);
+    }else {
+      colFour.push(parseInt(col4[i].innerHTML));
+    }
+  }
+  var colFive = [];
+  var col5 = document.getElementsByClassName('col-5');
+  for(var i = 0; i < 5; i++){
+    if (col5[i].classList.contains('inside-deselect')) {
+      colFive.push(0);
+    }else {
+      colFive.push(parseInt(col5[i].innerHTML));
+    }
+  }
+  var colNumbers = [colOne, colTwo, colThree, colFour, colFive];
+  return colNumbers;
 }
+
+function getRowArrays(){ //makes an array of row arrays
+  // Each row as array of integers
+  var rowOne = [];
+  var row1 = document.getElementsByClassName('row-1');
+  for(var i = 0; i < 5; i++){
+    if (row1[i].classList.contains('inside-deselect')) {
+      rowOne.push(0);
+    }else {
+      rowOne.push(parseInt(row1[i].innerHTML));
+    }
+  }
+  var rowTwo = [];
+  var row2 = document.getElementsByClassName('row-2');
+  for(var i = 0; i < 5; i++){
+    if (row2[i].classList.contains('inside-deselect')) {
+      rowTwo.push(0);
+    }else {
+      rowTwo.push(parseInt(row2[i].innerHTML));
+    }
+  }
+  var rowThree = [];
+  var row3 = document.getElementsByClassName('row-3');
+  for(var i = 0; i < 5; i++){
+    if (row3[i].classList.contains('inside-deselect')) {
+      rowThree.push(0);
+    }else {
+      rowThree.push(parseInt(row3[i].innerHTML));
+    }
+  }
+  var rowFour = [];
+  var row4 = document.getElementsByClassName('row-4');
+  for(var i = 0; i < 5; i++){
+    if (row4[i].classList.contains('inside-deselect')) {
+      rowFour.push(0);
+    }else {
+      rowFour.push(parseInt(row4[i].innerHTML));
+    }
+  }
+  var rowFive = [];
+  var row5 = document.getElementsByClassName('row-5');
+  for(var i = 0; i < 5; i++){
+    if (row5[i].classList.contains('inside-deselect')) {
+      rowFive.push(0);
+    }else {
+      rowFive.push(parseInt(row5[i].innerHTML));
+    }
+  }
+  var rowNumbers = [rowOne, rowTwo, rowThree, rowFour, rowFive];
+  return rowNumbers;
+}
+
+function getOutsideSum(){
+  // Variable for the outside-sum (outermost cells / array of 20 elements)
+  var outsideSum = document.getElementsByClassName('outside-sum');
+  // sum for top outsideSum (row 1 of 9x9 / first 5 elements)
+  for (var i = 0; i < outsideSum.length/4; i++) {
+    outsideSum[i].innerHTML = getColumnArrays()[i].reduce(function(a, b) { return a + b; });
+  }
+  // sum for left outsideSum (column 1 of 9x9 / next 5 elements with increment of 2)
+  var i,j;
+  for (i = 5, j = 0; i < outsideSum.length*3/4, j < 5; i+=2, j++) {
+    outsideSum[i].innerHTML = getRowArrays()[j].reduce(function(a, b) { return a + b; });
+  }
+  // sum for bottom outsideSum (row 9 of 9x9)
+  outsideSum[15].innerHTML = outsideSum[0].innerHTML;
+  outsideSum[16].innerHTML = outsideSum[1].innerHTML;
+  outsideSum[17].innerHTML = outsideSum[2].innerHTML;
+  outsideSum[18].innerHTML = outsideSum[3].innerHTML;
+  outsideSum[19].innerHTML = outsideSum[4].innerHTML;
+  // sum for right outsideSum (column 9 of 9x9)
+  outsideSum[6].innerHTML = outsideSum[5].innerHTML;
+  outsideSum[8].innerHTML = outsideSum[7].innerHTML;
+  outsideSum[10].innerHTML = outsideSum[9].innerHTML;
+  outsideSum[12].innerHTML = outsideSum[11].innerHTML;
+  outsideSum[14].innerHTML = outsideSum[13].innerHTML;
+  return;
+}
+
+getOutsideSum();
+insideClick();
+// var allCellTags = ['col-1', 'col-2', 'col-3', 'col-4', 'col-5', 'row-1', 'row-2', 'row-3', 'row-4', 'row-5']
+// function sumRemove(insideTag){
+//   for (var i = 0; i < allCellTags.length; i++) {
+//     if (insideTag.classList.contains(allCellTags[i])) {
+//       insideTag.classList.remove(allCellTags[i]);
+//     }else{
+//       return;
+//     }
+//   }
+//   return;
+// }
+
+
+
+function insideClick(){ // Selecting/Deselecting inside numbers
+  // Inside 5x5 cells with 25 elements
+  var insideNumber = document.getElementsByClassName('inside');
+  for (var i = 0; i < insideNumber.length; i++) {
+    insideNumber[i].addEventListener('click', function(event){
+      if (this.classList.contains('inside-deselect')) { //select
+        this.classList.remove('inside-deselect');
+      }else{ //deselect
+        this.classList.add('inside-deselect');
+      }
+      getOutsideSum();
+      // console.log(getColumnArrays());
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+// Checking if equal
